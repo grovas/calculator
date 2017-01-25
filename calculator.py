746295557cd807a18b554fb931b65238be6195ba
grovas <arczir@gmail.com>
2017-01-25 12:32:56 +0100

    "added correct version"

--- calculator.py
+++ calculator.py
@@ -4,26 +4,32 @@ This is a simple calculator application written for Python 3.x and for exercise 
 As input program expect 2 numbers.
 As output user can choose from several mathematical operation.
 
+Logic layer:
+    -
+
 ver. 0.1
 
 Grovas - Artur Rozgowski <arczir@gmail.com>
 
 """
-import sys
 
 print("Calculator App\n")
 
 
-# Function reads input from the user and checks if number is int/float type
 def get_numbers():
-    input_numbers = []
+    """
+    Function reads input from the user and checks if number is int/float type
+    :return: list of 2 numbers
+    """
+    input_numbers = []  # empty list to store 2 numbers
     for i in range(1, 3):
         while True:
             try:
                 print("Enter number " + str(i) + " :")
                 l = input()
+                # to list we save variable float from input
                 input_numbers.insert(i, float(l))
-            except ValueError:
+            except ValueError:  # if not numbers entered program will wait for correction
                 print("You don't entered the number!")
                 continue
             else:
@@ -31,10 +37,13 @@ def get_numbers():
     return input_numbers
 
 
-# Function takes only 1st letter entered by user
 def get_decision():
-    act=""
-    while True: # loop control proper entered decision by user
+    """
+    Function ask user to make a decision what calculation todo.
+    Function takes only 1st letter entered by user and lower this letter
+    :return:
+    """
+    while True:  # loop control proper entered decision by user
         act = input("\nWhat do you want to calculate with 2 digits:\n"
                     "Press 'A' for Addiction or\n"
                     "Press 'S' for Subtract or\n"
@@ -48,9 +57,14 @@ def get_decision():
     return act
 
 
-# Function checks decision made by user
 def chosen_action(act, list_number):
-    result = [0,""]
+    """
+    Function checks decision made by user
+    :param act:
+    :param list_number:
+    :return: list with calculation result and name of calculation required for print result
+    """
+    result = [0,""]  # list to store result and name of mathematical function
     while act not in ('x', 'X', 'q', 'Q'):
         if act == 'a':
             result[0] = add(list_number)
@@ -58,7 +72,7 @@ def chosen_action(act, list_number):
             break
         elif act == 's':
             result[0] = sub(list_number)
-            result[1] = 'substract'
+            result[1] = 'subtract'
             break
         elif act == 'm':
             result[0] = mul(list_number)
@@ -75,19 +89,23 @@ def chosen_action(act, list_number):
     return result
 
 
-# Function print out formatted result
 def print_result(numbers, result):
-    doneCalculation = ""
-    print("Result of {} number {} and number {} is: {}\n".
+    """
+    Function print out formatted result
+    Output printed 'result' is formatted to 2 digits after decimal dot
+    Function returns answer 'on exit from program' - it takes first lower letter [0]
+    """
+    print("Result of {0} number {1} and number {2} is: {3:0.2f}\n".
           format(result[1], numbers[0], numbers[1], result[0]))
-    doneCalculation = input("Do you want to exit? Press 'y' or 'n' ")[0]
-    return doneCalculation
+    calculation_done = input("Do you want to exit? Press 'y' or 'n' ")[0].lower()
+    return calculation_done
 
 
-# Function check if user want to calculate another set of numbers or exit program
-def new_calculation():
-    new_numbers = ""
-    new_numbers = input("Do you want to check other numbers? ('y' or 'n')")[0]
+def other_numbers():
+    """
+    Function check and return if user want to calculate another set of numbers
+    """
+    new_numbers = input("Do you want to check other numbers? ('y' or 'n')")[0].lower()
     return new_numbers
 
 
@@ -98,7 +116,7 @@ def add(numbers):
     return result
 
 
-# Function substrack
+# Function subtract
 def sub(numbers):
     result = 0
     result = numbers[0] - numbers[1]
@@ -128,42 +146,26 @@ def mod(numbers):
 
 # Main function
 def main():
-    first_calculation = ""
-    whatCalculationToDo = ""
-    #end_program = False
-    decision = ""
-    list_number = []  # program save input numbers to the list
-
-    list_number = get_numbers()
-
-    decision = get_decision()
-
-    resultFromCalculation = chosen_action(decision, list_number)
-
-    end_program = print_result(list_number, resultFromCalculation)
+    end_program = 'n'
+    first_calculation = True
+    list_number = []  # global list of calculated input numbers
+    new_numbers = ""
     while True:
-        end_program = False
-        if end_program == False:  # check if user want to end program
-            if new_calculation() == 'y':  # check if continue with a new numebers
-                list_number = []
+        if end_program == 'n':  # checks if user want to end program
 
+            if first_calculation: # checks if it is first loop of program
                 list_number = get_numbers()
-                decision = get_decision()
-
-                resultFromCalculation = chosen_action(decision, list_number)
-
-                end_program = print_result(list_number, resultFromCalculation)
-
-            else:
-                decision = get_decision() # else continue with old numbers
-
-                resultFromCalculation = chosen_action(decision, list_number)
-
-                end_program = print_result(list_number, resultFromCalculation)
-
+            if not first_calculation:
+                new_numbers = other_numbers()  # check if user want to new numbers
+            if new_numbers == 'y':
+                list_number = get_numbers()
+            decision = get_decision()
+            calculation_result = chosen_action(decision, list_number)
+            end_program = print_result(list_number, calculation_result)
+            first_calculation = False
         else:
             break
 
-# The main entry
 if __name__ == '__main__':
-    sys.exit(main())
+    """ The main entry of program """
+    main()
